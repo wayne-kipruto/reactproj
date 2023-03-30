972e9590a1431efb5e163e1776c56e91b58cb171
wayne-kipruto <wayne.kipruto@strathmore.edu>
2023-03-30 23:53:00 +0300

    responsiveness

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import './navbar.css';
-import { RiMenu3Line, RiCloseLin} from 'react-icons/ri'; 
-import logo from '../../assets/logo.svg';
+import React,{useState} from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+
+const Menu=() => (
+  <>
+  <p>
+  <a href="#home">Home</a>
+</p>
+<p>
+  <a href="wgpt3">What is GPT 3?</a>
+</p>
+<p>
+  <a href="#possibility">Open AI</a>
+</p>
+<p>
+  <a href="#features">Case Studies</a>
+</p>
+<p>
+  <a href="#blog">Library</a>
+</p>
+</>
+)
 
 const Navbar = () => {
+  const {toggleMenu,setToggleMenu} = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar_links">
         <div className="gpt3__navbar_links_logo">
-          <img src={logo} alt="logo"/>
-          </div>
-          <div className="gpt3__navbar_links_container">
-            <p><a href="#home">Home</a></p>
-            <p><a href="wgpt3">What is GPT?</a></p>
-            <p><a href="#possibility">Possibility</a></p>
-            <p><a href="#features">Features</a></p>
-            <p><a href="#blog">Library</a></p>
-          </div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar_links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar_sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="gpt3__navbar_menu">
+        {toggleMenu 
+        ? <RiCloseLine color="fff" size={27} onclick={()=> setToggleMenu(false)} />
+          : <RiMenu3Line color="fff" size={27} onclick={()=> setToggleMenu(true)}/>
+          }
+          {toggleMenu && (
+            <div className="gpt3__navbar_menu_container scale-up-center">
+              <div className="gpt3__navbar_menu_container_links">
+              <Menu />
+              <div className="gpt3__navbar_menu_links_sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+              </div>
+            </div>
+          )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
